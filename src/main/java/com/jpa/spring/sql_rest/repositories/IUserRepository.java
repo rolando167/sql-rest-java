507634bb0d692a3754b86c31f46b7043ede7f95d
Rolando HR <rolando167@hotmail.com>
2024-06-26 22:57:23 +0200

    RH 26 - ca ☕ 🦁

--- src/main/java/com/jpa/spring/sql_rest/repositories/IUserRepository.java
+++ src/main/java/com/jpa/spring/sql_rest/repositories/IUserRepository.java
@@ -1,9 +1,13 @@
 package com.jpa.spring.sql_rest.repositories;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.jpa.spring.sql_rest.models.User;
 
+@Repository
 public interface IUserRepository extends JpaRepository<User, Integer>{
-
+    
 }
